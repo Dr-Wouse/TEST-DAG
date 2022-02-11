8bad34bd37c84f4b45bdf5b21f9e8c029606b917
Dr Wouse <e.gon66@outlook.fr>
2022-02-12 00:26:45 +0100

    Correction

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="bg-slate-600 text-white shadow-lg">
       <div className=" flex flex-wrap items-center justify-between lg:container px-4 py-6 mx-auto md:flex-no-wrap md:px-6">
-        <h1 className="font-bold">Classement des Employé de DAG SYSTEME</h1>
+        <h1 className="font-bold">Classement des Employé de DAG SYSTEM</h1>
       </div>
     </header>
   );
